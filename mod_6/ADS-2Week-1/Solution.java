7bdf58f08c2761692d617859d2b60959c5e74d0d
Surya <suryasaginala@msitprogram.net>
2018-11-03 13:17:36 +0530

    "ads2_m6_a1_1: 3/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- mod_6/ADS-2Week-1/Solution.java
+++ mod_6/ADS-2Week-1/Solution.java
@@ -1,11 +1,31 @@
 import java.util.*;
+/**
+ * Class for page rank.
+ */
 class PageRank {
+	/**
+	 * Diagraph.
+	 */
 	private Digraph gr;
+	/**
+	 * array values.
+	 */
 	private double[] values;
+	/**
+	 * Pr values.
+	 */
 	private double[] prValues;
-	double test;
+	/**
+	 * test conducting.
+	 */
+	double check;
 
 
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      graph  The graph
+	 */
 	PageRank(Digraph graph) {
 		this.gr = graph;
 		this.gr = graph;
@@ -16,15 +36,18 @@ class PageRank {
 		}
 		maxPr();
 	}
+	/**
+	 * maxPr value.
+	 */
 	void maxPr() {
 	Digraph tempgr = gr.reverse();
 		for (int i = 0; i < 1000; i++) {
 			for (int j = 0; j < gr.V(); j++) {
-				test = 0.0;
+				check = 0.0;
 				for (int each : tempgr.adj(j)) {
-					test += (values[each]) / (double)(gr.outdegree(each));
+					check += (values[each]) / (double)(gr.outdegree(each));
 				}
-				prValues[j] = test;
+				prValues[j] = check;
 			}
 			values = prValues.clone();
 		}
@@ -34,12 +57,23 @@ class PageRank {
 	}
 }
 
+/**
+ * Class for web search.
+ */
 class WebSearch {
 
 }
 
 
+/**
+ * Class for solution.
+ */
 public class Solution {
+	/**
+	 * main function.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		// read the first line of the input to get the number of vertices
@@ -51,14 +85,21 @@ public class Solution {
 		for (int i = 0; i < a; i++) {
 			String[] tokens = sc.nextLine().split(" ");
 			for (int j = 1; j < tokens.length; j++) {
-				g.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[j]));	
+				g.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[j]));
+				if (tokens.length == 1) {
+					for (int k = 0; k < g.V(); k++) {
+						if (k != Integer.parseInt(tokens[0])) {
+							g.addEdge(Integer.parseInt(tokens[0]), k);
+						}
+					}
+				}	
 			}
 		}
 		System.out.println(g);
 		// Create page rank object and pass the graph object to the constructor
 		PageRank pr = new PageRank(g);
 		// print the page rank object
-		// This part is only for the final test case
+		// This part is only for the final check case
 		
 		// File path to the web content
 		String file = "WebContent.txt";
