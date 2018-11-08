20796f5057357f8ee3edc73951f9b72e59011cfd
Surya <suryasaginala@msitprogram.net>
2018-11-08 12:35:54 +0530

    "ads2_m10_a1_1: 2/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ mod_10/Seam Carving/SeamCarver.java
@@ -0,0 +1,61 @@
+ 
+public class SeamCarver {
+	Picture pict;
+	// create a seam carver object based on the given picture
+	public SeamCarver(Picture picture) {
+		this.pict = picture;
+	}
+	// current picture
+	public Picture picture() {
+		return pict;
+	}
+	// width of current picture
+	public int width() {
+		return pict.width();
+	}
+
+	// height of current picture
+	public int height() {
+		return pict.height();
+	}
+
+	// energy of pixel at column x and row y
+	public double energy(int x, int y) {
+		double energ = 0;
+		if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1) {
+			double n = 1000;
+			return n;
+		} else {
+			int rx = pict.get(x, y + 1).getRed() - pict.get(x, y - 1).getRed();
+			int ry = pict.get(x + 1, y).getRed() - pict.get(x - 1, y).getRed();
+			int bx = pict.get(x, y + 1).getBlue() - pict.get(x, y - 1).getBlue();
+			int by = pict.get(x + 1, y).getBlue() - pict.get(x - 1, y).getBlue();
+			int gx = pict.get(x, y + 1).getGreen() - pict.get(x, y - 1).getGreen();
+			int gy = pict.get(x + 1, y).getGreen() - pict.get(x, y - 1).getGreen();
+			double delxsq = (rx) * rx + bx * bx + gx * gx;
+			double delysq = (ry) * ry + by * by + gy * gy;
+			energ = Math.sqrt(delxsq + delysq);
+		}
+		return energ;
+	}
+
+	// sequence of indices for horizontal seam
+	public int[] findHorizontalSeam() {
+		return new int[0];
+	}
+
+	// sequence of indices for vertical seam
+	public int[] findVerticalSeam() {
+		return new int[0];
+	}
+
+	// remove horizontal seam from current picture
+	public void removeHorizontalSeam(int[] seam) {
+
+	}
+
+	// remove vertical seam from current picture
+	public void removeVerticalSeam(int[] seam) {
+
+	}
+}
