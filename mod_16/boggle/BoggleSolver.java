be13b2c20b0ca7694f466e2b69154e6d3855c3c9
Surya <suryasaginala@msitprogram.net>
2018-11-15 16:43:18 +0530

    "ads2_m16_a1_1: 2/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ mod_16/boggle/BoggleSolver.java
@@ -0,0 +1,24 @@
+public class BoggleSolver {
+	TST t;
+	// Initializes the data structure using the given array of strings as the dictionary.
+	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+	public BoggleSolver(String[] dictionary) {
+		t = new TST();
+		int i = 0;
+		for (String each: dictionary) {
+			t.put(each, i);
+			i++;
+		}
+	}
+
+	// Returns the set of all valid words in the given Boggle board, as an Iterable.
+	public Iterable<String> getAllValidWords(BoggleBoard board) {
+		return new Bag<String>();
+	}
+
+	// Returns the score of the given word if it is in the dictionary, zero otherwise.
+	// (You can assume the word contains only the uppercase letters A through Z.)
+	public int scoreOf(String word) {
+		return 0;
+	}
+}
