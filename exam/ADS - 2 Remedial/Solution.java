a652ac7c0965f983d3b1c3f2458f306a39d49251
Surya <suryasaginala@msitprogram.net>
2019-02-23 15:28:20 +0530

    "ads2_remedial_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- exam/ADS - 2 Remedial/Solution.java
+++ exam/ADS - 2 Remedial/Solution.java
@@ -9,18 +9,48 @@ class Solution {
 		Digraph di = new Digraph(a);
 		for (int i =0; i < b; i++) {
 			String[] str = sc.nextLine().split(" ");
-			di.addEdge(Integer.parseInt(str[0]), Integer.parseInt(str[0]));
+			di.addEdge(Integer.parseInt(str[0]), Integer.parseInt(str[1]));
 		}
-		// DirectedCycle dy = new DirectedCycle(di);
-		String s = "";
-		for (int j = 0; j < a; j++) {
-			DepthFirstDirectedPaths dfs = new DepthFirstDirectedPaths(di, j);
-			
-			if (dfs.hasPathTo(j)) {
-				s += j+", ";
+		DirectedCycle dy = new DirectedCycle(di);
+		int[] s = new int[a];
+		if (dy.hasCycle()) {
+			int i = 0;
+			for (int each : dy.cycle()) {
+				s[i] = each;
+				i++;
 			}
-			
+			Arrays.sort(s);
+			String str = "";
+			int n = removeDuplicates(s, s.length);
+			for (int j = 0; j < n; j++) {
+				str += s[j]+", ";
+			}
+			System.out.println(str.substring(0,str.length()-2));
+		}else{
+			System.out.println("No Self Beneficiaries.");
 		}
-		System.out.println(s);
 	}
+
+    static int removeDuplicates(int arr[], int n) {  
+        if (n==0 || n==1) 
+            return n; 
+       
+        int[] temp = new int[n]; 
+          
+        int j = 0; 
+        for (int i=0; i<n-1; i++) {
+            if (arr[i] != arr[i+1]){
+
+                temp[j++] = arr[i]; 
+            } 
+        } 
+        temp[j++] = arr[n-1];    
+          
+        for (int i=0; i<j; i++){
+
+            arr[i] = temp[i]; 
+        } 
+       
+        return j; 
+    }
 }
